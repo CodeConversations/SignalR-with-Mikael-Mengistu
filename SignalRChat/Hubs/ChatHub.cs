e6adb505c601f3e69873d17b2617f99d6307f4a0
Mikael Mengistu <mimengis@microsoft.com>
2017-08-02 15:03:15 -0700

    initial commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
